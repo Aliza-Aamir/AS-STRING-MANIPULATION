9940a47009e1369ccf7757da34df1314ccacc902
Aliza Aamir <73904031+Aliza-Aamir@users.noreply.github.com>
2020-11-21 18:49:57 +0500

    Add files via upload

---
+++ Character counter.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim userinput As String
+        Dim x As Integer
+
+
+        Console.WriteLine("Please input a string of characters and this algorithm will count it.")
+        userinput = Console.ReadLine()
+        x = Len(userinput)
+        Console.WriteLine("The number of entered characters including spaces is: " & x)
+
+        Console.Read()
+    End Sub
+
+End Module
+
