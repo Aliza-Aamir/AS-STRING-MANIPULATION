e19246440e153cc15d1b50d15b8c18105e42c3a9
Aliza Aamir <73904031+Aliza-Aamir@users.noreply.github.com>
2020-11-27 15:16:00 +0500

    Add files via upload

---
+++ verbing.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim userinput As String
+        Dim count As Integer
+        Dim isPresent As Boolean
+
+        userinput = ""
+        count = 0
+        isPresent = True
+
+        Console.WriteLine("The purpose of this program is to take userinput greater than 3 characters and adding '-ing' to it. However, if the userinput already has '-ing' in it then the program will add '-ly' to it.")
+        Console.WriteLine("Please enter your character:")
+        userinput = Console.ReadLine()
+
+            If Len(userinput) < 3 Then
+                Console.WriteLine(userinput)
+
+
+        ElseIf Len(userinput) >= 3 Then
+
+            If userinput.Contains("ing") Then
+                isPresent = True
+                Console.WriteLine(userinput & "ly")
+
+            Else
+                isPresent = False
+                Console.WriteLine(userinput & "ing")
+            End If
+
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
