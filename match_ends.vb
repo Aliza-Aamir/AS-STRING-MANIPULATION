bc988aab365f5499aa2cbbc18838725f6d761d20
Aliza Aamir <73904031+Aliza-Aamir@users.noreply.github.com>
2020-11-27 21:23:42 +0500

    Add files via upload

---
+++ match_ends.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim data, entereddata As String
+        Dim firstchar, lastchar, x As Char
+        Dim counter As Integer = 0
+        entereddata = ""
+        counter = 0
+        data = "ABCDEFGHIJKLMNOPQRST"
+
+        Console.WriteLine("The data: ABCDEFGHIJKLMNOPQRST.")
+        Console.WriteLine("Enter data:")
+        entereddata = Console.ReadLine()
+
+        For counter = 0 To Len(entereddata)
+            If Len(entereddata) >= 2 Then
+
+                firstchar = Mid(entereddata, 1, 1)
+                lastchar = Left(entereddata, 1)
+                If firstchar = lastchar Then
+                    counter = counter + 1
+
+                End If
+                Console.WriteLine(counter & firstchar)
+
+
+            Else : Console.WriteLine("error")
+            End If
+
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
