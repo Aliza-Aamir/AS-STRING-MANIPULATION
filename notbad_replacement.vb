a1158a0da3dfd59297cafbefa199172e866557e1
Aliza Aamir <73904031+Aliza-Aamir@users.noreply.github.com>
2020-11-27 11:56:03 +0500

    Add files via upload

---
+++ notbad_replacement.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim userinput, finalstring As String
+        Dim count As Integer
+
+        userinput = ""
+        finalstring = 0
+
+        Console.WriteLine("This program changes the words: 'not bad' within your entered string with 'good'.")
+        Console.WriteLine("Please enter your input:")
+        userinput = Console.ReadLine()
+
+        For count = 1 To Len(userinput)
+            finalstring = userinput.Replace("not bad", "good")
+
+        Next
+        Console.WriteLine("Your output is: " & finalstring)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
